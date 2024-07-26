8a0f916e8b8aa65c7b17c7944369a2c935a81c63
muhammadarii <aripurnomo606@gmail.com>
2024-07-26 21:12:54 +0700

    Update Progress:make localstoragebrowser

--- src/data/data.js
+++ src/data/data.js
@@ -3,14 +3,14 @@ export const login = [
     id: 1,
     username: "aripu",
     password: "123456",
-    token: "k2h3g423g4lj2h34l2h3j4232l3k4h2kh23g43k",
-    refresh_token: "2j2h42hg34uoi2g3ou4g2o3u4v2ov42oiuy4",
+    token: process.env.REACT_APP_TOKEN,
+    refresh_token: process.env.REACT_APP_REFRESH_TOKEN,
   },
   {
     id: 2,
     username: "surya",
     password: "123456",
-    token: "k2h3g423g4lj2h34l2h3j4232l3k4h2kh23g43k",
-    refresh_token: "2j2h42hg34uoi2g3ou4g2o3u4v2ov42oiuy4",
+    token: process.env.REACT_APP_TOKEN,
+    refresh_token: process.env.REACT_APP_REFRESH_TOKEN,
   },
 ];
